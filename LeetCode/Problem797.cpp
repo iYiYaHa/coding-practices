d5ad0e7c0d150885cde9089424c36d7f6770dba3
WalkerJG <zhangyjM@outlook.com>
2018-10-17 14:07:38 +0800

    Problem 797 solved.

--- LeetCode/Problem797.cpp
+++ LeetCode/Problem797.cpp
@@ -25,7 +25,7 @@
 //
 //#include <iostream>
 //#include <vector>
-//#include <string>
+//#include <stack>
 //class Solution {
 //private:
 //	// Recursive
@@ -43,6 +43,56 @@
 //			}
 //		}
 //		return res;
+//	}
+//
+//	//DFS using stack
+//	std::vector<std::vector<int>> pathBetweenNodesDFS(std::vector<std::vector<int>> &graph, int src, int dst) {
+//		std::stack<int> stack;
+//		std::vector<std::vector<int>> res;
+//		std::vector<int> path;
+//		path.push_back(src);
+//		stack.push(src);
+//		for (int node : graph[src]) {
+//			stack.push(node);
+//		}
+//		while (!stack.empty()) {
+//			int currNode = stack.top();
+//			if (currNode == path.back()) {
+//				if (currNode == dst) {
+//					res.push_back(path);
+//				}
+//				path.pop_back();
+//				stack.pop();
+//				continue;
+//			}
+//			path.push_back(currNode);
+//			for (int node : graph[currNode]) {
+//				stack.push(node);
+//			}
+//		}
+//		return res;
+//	}
+//
+//	//DFS recursive
+//	std::vector<std::vector<int>> pathBetweenNodesRecursive(std::vector<std::vector<int>> &graph, int src, int dst) {
+//		std::vector<int> path;
+//		std::vector<std::vector<int>> paths;
+//		dfs(graph, paths, path, src, dst);
+//		return paths;
+//	}
+//
+//	void dfs(std::vector<std::vector<int>> &graph, 
+//							std::vector<std::vector<int>> &paths,std::vector<int> &path, int src, int dst) {
+//		path.push_back(src);
+//		if (src == dst) {
+//			paths.push_back(path);
+//		}
+//		else {
+//			for (int node : graph[src]) {
+//				dfs(graph, paths, path, node, dst);
+//			}
+//		}
+//		path.pop_back();
 //	}
 //public:
 //	std::vector<std::vector<int>> allPathsSourceTarget(std::vector<std::vector<int>>& graph) {
