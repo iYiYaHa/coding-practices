29728710f1715e265923c10a09778c2713c376e1
WalkerJG <zhangyjM@outlook.com>
2019-03-12 13:53:33 +0800

    Delete the if-statement in solution B.

--- LeetCode/474.ones-and-zeroes.cpp
+++ LeetCode/474.ones-and-zeroes.cpp
@@ -98,19 +98,15 @@ public:
 	int findMaxForm(std::vector<std::string> &strs, int m, int n)
 	{
 		std::vector<std::vector<int>> dp(m + 1, std::vector<int>(n + 1, 0));
-
 		for (int i = 1; i <= strs.size(); ++i)
 		{
 			int ones = std::count_if(strs[i - 1].begin(), strs[i - 1].end(), [](unsigned char c) { return c == '1'; });
 			int zeroes = std::count_if(strs[i - 1].begin(), strs[i - 1].end(), [](unsigned char c) { return c == '0'; });
-			for (int j = m; j >= 0; --j)
+			for (int j = m; j >= zeroes; --j)
 			{
-				for (int k = n; k >= 0; --k)
+				for (int k = n; k >= ones; --k)
 				{
-					if (j >= zeroes && k >= ones)
-					{
-						dp[j][k] = std::max(dp[j][k], dp[j - zeroes][k - ones] + 1);
-					}
+					dp[j][k] = std::max(dp[j][k], dp[j - zeroes][k - ones] + 1);
 				}
 			}
 		}
