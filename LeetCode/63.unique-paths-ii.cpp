9703d35483ab613356cccf78c12c66b8094a9090
Youjie Zhang <zhangyjM@outlook.com>
2019-04-09 10:29:56 +0800

    Problem 63 solved.

---
+++ LeetCode/63.unique-paths-ii.cpp
@@ -0,0 +1,71 @@
+/*
+ * @lc app=leetcode id=63 lang=cpp
+ *
+ * [63] Unique Paths II
+ *
+ * https://leetcode.com/problems/unique-paths-ii/description/
+ *
+ * algorithms
+ * Medium (33.30%)
+ * Total Accepted:    191.2K
+ * Total Submissions: 574.1K
+ * Testcase Example:  '[[0,0,0],[0,1,0],[0,0,0]]'
+ *
+ * A robot is located at the top-left corner of a m x n grid (marked 'Start' in
+ * the diagram below).
+ *
+ * The robot can only move either down or right at any point in time. The robot
+ * is trying to reach the bottom-right corner of the grid (marked 'Finish' in
+ * the diagram below).
+ *
+ * Now consider if some obstacles are added to the grids. How many unique paths
+ * would there be?
+ *
+ *
+ *
+ * An obstacle and empty space is marked as 1 and 0 respectively in the grid.
+ *
+ * Note: m and n will be at most 100.
+ *
+ * Example 1:
+ *
+ *
+ * Input:
+ * [
+ * [0,0,0],
+ * [0,1,0],
+ * [0,0,0]
+ * ]
+ * Output: 2
+ * Explanation:
+ * There is one obstacle in the middle of the 3x3 grid above.
+ * There are two ways to reach the bottom-right corner:
+ * 1. Right -> Right -> Down -> Down
+ * 2. Down -> Down -> Right -> Right
+ *
+ *
+ */
+class Solution {
+public:
+  int uniquePathsWithObstacles(vector<vector<int>> &obstacleGrid) {
+    int m = obstacleGrid.size();
+    if (m <= 0)
+      return 0;
+    int n = obstacleGrid[0].size();
+    if (n <= 0)
+      return 0;
+    vector<vector<long>> dp(m, vector<long>(n, 0));
+    for (int i = 0; i < m; ++i) {
+      for (int j = 0; j < n; ++j) {
+        if (i == 0 && j == 0)
+          dp[i][i] = obstacleGrid[i][j] ? 0 : 1;
+        else {
+          long left = i - 1 >= 0 ? dp[i - 1][j] : 0;
+          long above = j - 1 >= 0 ? dp[i][j - 1] : 0;
+          dp[i][j] = obstacleGrid[i][j] == 1 ? 0 : left + above;
+        }
+      }
+    }
+    return dp[m - 1][n - 1];
+  }
+};
