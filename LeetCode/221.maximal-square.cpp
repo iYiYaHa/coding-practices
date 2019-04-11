bf4efcadacd000d56da75ccfe25dbbbd7cbe29ef
Youjie Zhang <zhangyjM@outlook.com>
2019-04-11 12:02:42 +0800

    Modified the logic a little bit.

--- LeetCode/221.maximal-square.cpp
+++ LeetCode/221.maximal-square.cpp
@@ -40,16 +40,16 @@ public:
     if (m <= 0)
       return 0;
     vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
-    int ans = 0;
+    int maxLen = 0;
     for (int i = 1; i <= n; ++i) {
       for (int j = 1; j <= m; ++j) {
         if (matrix[i - 1][j - 1] == '1')
           dp[i][j] =std::min(dp[i - 1][j - 1], std::min(dp[i - 1][j], dp[i][j - 1])) +1;
         else
           dp[i][j] = 0;
-        ans = std::max(ans,dp[i][j]*dp[i][j]);
+        maxLen = std::max(maxLen,dp[i][j]);
       }
     }
-    return ans;
+    return maxLen*maxLen;
   }
 };
