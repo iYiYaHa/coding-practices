5cfb1b1b181613a6988e0bf6e4051cf0dd0b8433
Youjie Zhang <zhangyjM@outlook.com>
2019-04-11 15:29:11 +0800

    Optimized on previous solution.

--- LeetCode/221.maximal-square.cpp
+++ LeetCode/221.maximal-square.cpp
@@ -28,28 +28,57 @@
  * Output: 4
  *
  */
-class Solution {
-    // Dynamic Programming
-    // It can be optimized to use less space.
+class SolutionA {
+  // Dynamic Programming
+  // It can be optimized to use less space.
 public:
   int maximalSquare(vector<vector<char>> &matrix) {
+    if (matrix.empty() || matrix[0].empty())
+      return 0;
     int n = matrix.size();
-    if (n <= 0)
-      return 0;
     int m = matrix[0].size();
-    if (m <= 0)
-      return 0;
     vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
     int maxLen = 0;
     for (int i = 1; i <= n; ++i) {
       for (int j = 1; j <= m; ++j) {
         if (matrix[i - 1][j - 1] == '1')
-          dp[i][j] =std::min(dp[i - 1][j - 1], std::min(dp[i - 1][j], dp[i][j - 1])) +1;
+          dp[i][j] =
+              std::min(dp[i - 1][j - 1], std::min(dp[i - 1][j], dp[i][j - 1])) +
+              1;
         else
           dp[i][j] = 0;
-        maxLen = std::max(maxLen,dp[i][j]);
+        maxLen = std::max(maxLen, dp[i][j]);
       }
     }
-    return maxLen*maxLen;
+    return maxLen * maxLen;
+  }
+};
+
+class Solution {
+    // Space-optimized dynamic programming
+    // Elegant optimization which worths understanding.
+public:
+  int maximalSquare(vector<vector<char>> &matrix) {
+    if (matrix.empty() || matrix[0].empty())
+      return 0;
+    int n = matrix.size();
+    int m = matrix[0].size();
+    int prev = 0, maxLen = 0; // dp[i-1][j-1]
+    vector<int> dp(m + 1, 0);
+    for (int i = 1; i <= n; ++i) {
+      for (int j = 1; j <= m; ++j) {
+        int temp = dp[j];
+        if (matrix[i - 1][j - 1] == '1') {
+          // dp[j]->dp[i-1][j]
+          // dp[j-1]->dp[i][j-1]
+          dp[j] = std::min(dp[j], std::min(dp[j - 1], prev)) + 1;
+        } else {
+          dp[j] = 0;
+        }
+        maxLen = std::max(maxLen, dp[j]);
+        prev = temp;
+      }
+    }
+    return maxLen * maxLen;
   }
 };
