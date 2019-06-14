199f8e3485d25ef8444ae71d4010933e0d123639
WalkerJG <zhangyjM@outlook.com>
2019-06-14 15:53:57 +0800

    Add solution with slow-fast pointers to problem 876.

--- LeetCode/876.middle-of-the-linked-list.cpp
+++ LeetCode/876.middle-of-the-linked-list.cpp
@@ -11,15 +11,33 @@
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
-class Solution
+class SolutionA
 {
 public:
     ListNode *middleNode(ListNode *head)
     {
         int len = 0;
         ListNode *cur = head;
-        for(;cur!=nullptr;cur=cur->next,++len);
-        for(cur=head,len=len/2;len!=0;--len,cur=cur->next);
+        for (; cur != nullptr; cur = cur->next, ++len)
+            ;
+        for (cur = head, len = len / 2; len != 0; --len, cur = cur->next)
+            ;
         return cur;
+    }
+};
+
+class SolutionB
+{
+    // Slow and fast pointers.
+public:
+    ListNode *middleNode(ListNode *head)
+    {
+        ListNode *slow = head, *fast = head;
+        while (fast && fast->next)
+        {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        return slow;
     }
 };
