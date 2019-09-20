eb74bb9eb0989851105a95b0bb2204a96ca8791b
WalkerJG <zhangyjM@outlook.com>
2019-09-20 20:56:40 +0800

    Problem 726 solved.

---
+++ LeetCode/726.number-of-atoms.cpp
@@ -0,0 +1,85 @@
+/*
+ * @lc app=leetcode id=726 lang=cpp
+ *
+ * [726] Number of Atoms
+ */
+
+// It's not worth wasting time on this problem.
+class Solution
+{
+private:
+    map<string, int> parseFormula(string formula)
+    {
+        int beg = 0, end = 0;
+        map<string, int> res;
+        while (end < formula.size())
+        {
+            end=beg+1;
+            map<string, int> subRes;
+            if (formula[beg] == '(')
+            { // subformula
+                int left_cnt = 1;
+                while (end < formula.size() && left_cnt != 0)
+                {
+                    if (formula[end] == '(')
+                        ++left_cnt;
+                    else if (formula[end] == ')')
+                    {
+                        --left_cnt;
+                    }
+                    ++end;
+                }
+                subRes = parseFormula(formula.substr(beg + 1, end - beg - 2));
+            }
+            else
+            {
+                while (end < formula.size() && !isdigit(formula[end]) && (beg == end || !isupper(formula[end])) && formula[end] != '(')
+                {
+                    ++end;
+                }
+                subRes.insert({formula.substr(beg, end - beg), 1});
+            }
+            // Get Count here
+            int cnt = 0;
+            while (end < formula.size() && isdigit(formula[end]))
+            {
+                cnt = cnt * 10 + (formula[end] - '0');
+                ++end;
+            }
+            cnt = cnt == 0 ? 1 : cnt;
+            // Merge two map together
+            for (auto i = subRes.begin(); i != subRes.end(); ++i)
+            {
+                string ele = i->first;
+                int ele_cnt = i->second;
+                if (res.find(ele) == res.end())
+                {
+                    res.insert({ele, ele_cnt * cnt});
+                }
+                else
+                {
+                    res[ele] += ele_cnt * cnt;
+                }
+            }
+            beg = end;
+        }
+        return res;
+    }
+
+public:
+    string countOfAtoms(string formula)
+    {
+        map<string, int> res = parseFormula(formula);
+        string resStr;
+        for (auto i = res.begin(); i != res.end(); ++i)
+        {
+            string ele = i->first;
+            int cnt = i->second;
+            string cntStr = to_string(cnt);
+            resStr += ele;
+            if (cnt != 1)
+                resStr += cntStr;
+        }
+        return resStr;
+    }
+};
