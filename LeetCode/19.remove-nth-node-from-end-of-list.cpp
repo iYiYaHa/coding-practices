24b4e7e1c1489056d707f3fc86deddc5becdacf5
WalkerJG <zhangyjM@outlook.com>
2020-03-12 00:22:51 +0800

    Add comments in problem 19.

--- LeetCode/19.remove-nth-node-from-end-of-list.cpp
+++ LeetCode/19.remove-nth-node-from-end-of-list.cpp
@@ -44,22 +44,58 @@ class SolutionB
 {
 public:
     ListNode *removeNthFromEnd(ListNode *head, int n)
-
     {
         ListNode fakeHead(-1);
         fakeHead.next = head;
         int len = 0;
         // Count the length
-        for (ListNode *cur = head; cur != nullptr; cur = cur->next, ++len);
+        for (ListNode *cur = head; cur != nullptr; cur = cur->next, ++len)
+            ;
 
         // Find the previous node
         ListNode *prev = &fakeHead;
         int cutPos = len - n;
-        for (; cutPos > 0; --cutPos, prev = prev->next);
+        for (; cutPos > 0; --cutPos, prev = prev->next)
+            ;
 
         ListNode *tmp = prev->next;
         prev->next = tmp->next;
         delete tmp;
+        return fakeHead.next;
+    }
+};
+
+class Solution
+{
+public:
+    ListNode *removeNthFromEnd(ListNode *head, int n)
+    {
+        if (head == nullptr || n <= 0)
+            return head;
+        ListNode fakeHead(0);
+        fakeHead.next = head;
+
+        ListNode *pBeg = &fakeHead, *pEnd = &fakeHead;
+        for (int i = 0; i < n; ++i)
+        {
+            if (pEnd->next != nullptr)
+            {
+                pEnd = pEnd->next;
+            }
+            else
+            {
+                return fakeHead.next;
+            }
+        }
+        while (pEnd->next != nullptr)
+        {
+            pBeg = pBeg->next;
+            pEnd = pEnd->next;
+        }
+        ListNode *tmp = pBeg->next;
+        pBeg->next = tmp->next;
+        delete tmp;
+
         return fakeHead.next;
     }
 };
