bfd740a3b37ae0ffa0f416579c70f73b86f387a6
iYiYaHa <zhangyjM@outlook.com>
2020-03-20 21:11:47 +0800

    Coding Interviews problem 11 solved.

--- LeetCode/11.minimum-in-rotated-array.cpp
+++ LeetCode/11.minimum-in-rotated-array.cpp
@@ -4,7 +4,7 @@ public:
     int minArray(vector<int>& numbers) {
         int left = 0, right = numbers.size()-1;
         while(left<right){
-            if(numbers[left]<numbers[right])break;
+            if(numbers[left]<numbers[right])break;// 相当于分情况讨论。这种情况就是没旋转的。
             int mid = left + (right-left)/2;
             if(numbers[left]<numbers[mid]){
                 left=mid+1;
@@ -19,6 +19,8 @@ public:
 };
 
 // ref:https://leetcode-cn.com/problems/xuan-zhuan-shu-zu-de-zui-xiao-shu-zi-lcof/solution/mian-shi-ti-11-xuan-zhuan-shu-zu-de-zui-xiao-shu-3/
+// 旋转点是右半边数组的最小值。
+// left 不一定是左半边的数组中的。但 right 一定在右半边的数组中。
 class SolutionB {
 public:
     int minArray(vector<int>& numbers) {
