79eb0335ed46e515cc101380d9a16f1c2b9df97d
339 <zhangyjM@outlook.com>
2020-08-15 10:32:31 +0800

    Update coding interviews 11.

--- CodingInterviews/java/11.minimum-in-a-rotated-array.java
+++ CodingInterviews/java/11.minimum-in-a-rotated-array.java
@@ -15,3 +15,24 @@ class Solution {
         return numbers[left];
     }
 }
+
+class Solution {
+    public int minArray(int[] nums) {
+        if(nums == null || nums.length <= 0)
+            return 0;
+        int left = 0, right = nums.length - 1;
+        while(left < right){
+            if(nums[left] < nums[right])
+                break;
+            int mid = left + (right-left)/2;
+            if(nums[left] > nums[mid]){
+                right = mid;
+            }else if(nums[left] < nums[mid]){
+                left = mid + 1;
+            }else{
+                ++left;
+            }
+        }
+        return nums[left];
+    }
+}
