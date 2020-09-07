a432dbd24b36c4cf2afb440cb05bda9d1340bae0
iYiYaHa <zhangyjM@outlook.com>
2020-09-07 11:26:19 +0800

    Problem 695 solved in Java.

---
+++ LeetCode/java/695.max-area-of-island.java
@@ -0,0 +1,72 @@
+/*
+ * @lc app=leetcode id=695 lang=java
+ *
+ * [695] Max Area of Island
+ *
+ * https://leetcode.com/problems/max-area-of-island/description/
+ *
+ * algorithms
+ * Medium (62.27%)
+ * Likes:    2144
+ * Dislikes: 83
+ * Total Accepted:    167.5K
+ * Total Submissions: 265.9K
+ * Testcase Example:  '[[1,1,0,0,0],[1,1,0,0,0],[0,0,0,1,1],[0,0,0,1,1]]'
+ *
+ * Given a non-empty 2D array grid of 0's and 1's, an island is a group of 1's
+ * (representing land) connected 4-directionally (horizontal or vertical.) You
+ * may assume all four edges of the grid are surrounded by water.
+ * 
+ * Find the maximum area of an island in the given 2D array. (If there is no
+ * island, the maximum area is 0.)
+ * 
+ * Example 1:
+ * 
+ * 
+ * [[0,0,1,0,0,0,0,1,0,0,0,0,0],
+ * ⁠[0,0,0,0,0,0,0,1,1,1,0,0,0],
+ * ⁠[0,1,1,0,1,0,0,0,0,0,0,0,0],
+ * ⁠[0,1,0,0,1,1,0,0,1,0,1,0,0],
+ * ⁠[0,1,0,0,1,1,0,0,1,1,1,0,0],
+ * ⁠[0,0,0,0,0,0,0,0,0,0,1,0,0],
+ * ⁠[0,0,0,0,0,0,0,1,1,1,0,0,0],
+ * ⁠[0,0,0,0,0,0,0,1,1,0,0,0,0]]
+ * 
+ * Given the above grid, return 6. Note the answer is not 11, because the
+ * island must be connected 4-directionally.
+ * 
+ * Example 2:
+ * 
+ * 
+ * [[0,0,0,0,0,0,0,0]]
+ * Given the above grid, return 0.
+ * 
+ * Note: The length of each dimension in the given grid does not exceed 50.
+ * 
+ */
+
+// @lc code=start
+class Solution {
+    public int maxAreaOfIsland(int[][] grid) {
+        int maxArea = 0;
+        for (int i = 0; i < grid.length; ++i) {
+            for (int j = 0; j < grid[0].length; ++j) {
+                maxArea = Math.max(maxArea, dfs(grid, i, j));
+            }
+        }
+        return maxArea;
+    }
+
+    private int dfs(int[][] grid, int i, int j) {
+        if (i < 0 || i >= grid.length || j < 0 || j >= grid[i].length)
+            return 0;
+        if (grid[i][j] == 0)
+            return 0;
+        grid[i][j] = 0;
+        return 1 + dfs(grid, i + 1, j) 
+        + dfs(grid, i - 1, j) 
+        + dfs(grid, i, j + 1) 
+        + dfs(grid, i, j - 1);
+    }
+}
+// @lc code=end
