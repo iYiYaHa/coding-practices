7f44bcb4ec73e3fa0fc0620e7a876d62c8978509
iYiYaHa <zhangyjM@outlook.com>
2020-10-12 10:37:49 +0800

    Problem 116 solved in Java.

---
+++ LeetCode/java/116.populating-next-right-pointers-in-each-node.java
@@ -0,0 +1,150 @@
+/*
+ * @lc app=leetcode id=116 lang=java
+ *
+ * [116] Populating Next Right Pointers in Each Node
+ *
+ * https://leetcode.com/problems/populating-next-right-pointers-in-each-node/description/
+ *
+ * algorithms
+ * Medium (44.46%)
+ * Likes:    2397
+ * Dislikes: 147
+ * Total Accepted:    397.5K
+ * Total Submissions: 857K
+ * Testcase Example:  '[1,2,3,4,5,6,7]'
+ *
+ * You are given a perfect binary tree where all leaves are on the same level,
+ * and every parent has two children. The binary tree has the following
+ * definition:
+ * 
+ * 
+ * struct Node {
+ * ⁠ int val;
+ * ⁠ Node *left;
+ * ⁠ Node *right;
+ * ⁠ Node *next;
+ * }
+ * 
+ * 
+ * Populate each next pointer to point to its next right node. If there is no
+ * next right node, the next pointer should be set to NULL.
+ * 
+ * Initially, all next pointers are set to NULL.
+ * 
+ * 
+ * 
+ * Follow up:
+ * 
+ * 
+ * You may only use constant extra space.
+ * Recursive approach is fine, you may assume implicit stack space does not
+ * count as extra space for this problem.
+ * 
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * 
+ * 
+ * Input: root = [1,2,3,4,5,6,7]
+ * Output: [1,#,2,3,#,4,5,6,7,#]
+ * Explanation: Given the above perfect binary tree (Figure A), your function
+ * should populate each next pointer to point to its next right node, just like
+ * in Figure B. The serialized output is in level order as connected by the
+ * next pointers, with '#' signifying the end of each level.
+ * 
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * The number of nodes in the given tree is less than 4096.
+ * -1000 <= node.val <= 1000
+ * 
+ * 
+ */
+
+// @lc code=start
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node next;
+
+    public Node() {}
+    
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, Node _left, Node _right, Node _next) {
+        val = _val;
+        left = _left;
+        right = _right;
+        next = _next;
+    }
+};
+*/
+
+class Solution {
+    public Node connect(Node root) {
+        Queue<Node> queue = new LinkedList<>();
+        if(root != null)
+            queue.offer(root);
+        while(!queue.isEmpty()){
+            Node prev = null;
+            int layerCnt = queue.size();
+            for(int i = 0;i < layerCnt; ++i){
+                Node node = queue.poll();
+                if(prev != null){
+                    prev.next = node;
+                }
+                prev = node;
+                if(node.left != null)queue.offer(node.left);
+                if(node.right != null) queue.offer(node.right);
+            }
+            if(prev != null)
+                prev.next = null;
+        }
+        return root;
+    }
+}
+
+class Solution {
+    public Node connect(Node root) {
+        if(root == null)
+            return null;
+        if(root.left != null){
+            root.left.next = root.right;
+            if(root.next != null){
+                root.right.next = root.next.left;
+            }
+        }
+        connect(root.left);
+        connect(root.right);
+        return root;
+    }
+}
+
+class Solution {
+    public Node connect(Node root) {
+        if(root == null)
+            return null;
+        Node levelStart = root;
+        while(levelStart != null){
+            Node cur = levelStart;
+            while(cur != null){
+                if(cur.left != null)cur.left.next = cur.right;
+                if(cur.right != null && cur.next != null) cur.right.next = cur.next.left;
+                cur = cur.next;
+            }
+            levelStart = levelStart.left;
+        }
+        return root;
+    }
+}
+// @lc code=end
+
