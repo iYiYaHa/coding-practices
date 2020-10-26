3b871837ca5284431bdf2314e71e4ab2125c4299
iYiYaHa <zhangyjM@outlook.com>
2020-10-26 22:27:58 +0800

    Problem 2 solved in Java.

---
+++ LeetCode/java/2.add-two-numbers.java
@@ -0,0 +1,90 @@
+/*
+ * @lc app=leetcode id=2 lang=java
+ *
+ * [2] Add Two Numbers
+ *
+ * https://leetcode.com/problems/add-two-numbers/description/
+ *
+ * algorithms
+ * Medium (33.62%)
+ * Likes:    9568
+ * Dislikes: 2412
+ * Total Accepted:    1.6M
+ * Total Submissions: 4.7M
+ * Testcase Example:  '[2,4,3]\n[5,6,4]'
+ *
+ * You are given two non-empty linked lists representing two non-negative
+ * integers. The digits are stored in reverse order, and each of their nodes
+ * contains a single digit. Add the two numbers and return the sum as a linked
+ * list.
+ * 
+ * You may assume the two numbers do not contain any leading zero, except the
+ * number 0 itself.
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: l1 = [2,4,3], l2 = [5,6,4]
+ * Output: [7,0,8]
+ * Explanation: 342 + 465 = 807.
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: l1 = [0], l2 = [0]
+ * Output: [0]
+ * 
+ * 
+ * Example 3:
+ * 
+ * 
+ * Input: l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]
+ * Output: [8,9,9,9,0,0,0,1]
+ * 
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * The number of nodes in each linked list is in the range [1, 100].
+ * 0 <= Node.val <= 9
+ * It is guaranteed that the list represents a number that does not have
+ * leading zeros.
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list. public class ListNode { int val; ListNode
+ * next; ListNode() {} ListNode(int val) { this.val = val; } ListNode(int val,
+ * ListNode next) { this.val = val; this.next = next; } }
+ */
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode res = new ListNode(0);
+        ListNode prev = res;
+        int carry = 0;
+        while (l1 != null || l2 != null || carry != 0) {
+            int a = l1 == null ? 0 : l1.val;
+            int b = l2 == null ? 0 : l2.val;
+            int val = a + b + carry;
+            carry = 0;
+            if (val >= 10) {
+                carry = 1;
+                val -= 10;
+            }
+            ListNode digit = new ListNode(val);
+            prev.next = digit;
+            prev = prev.next;
+            if (l1 != null)
+                l1 = l1.next;
+            if (l2 != null)
+                l2 = l2.next;
+        }
+        return res.next;
+    }
+}
+// @lc code=end
