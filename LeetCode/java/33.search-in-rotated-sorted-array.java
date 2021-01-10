ea9a978aee92ffd7cfb375d473d90f7a9892c98c
iYiYaHa <zhangyjM@outlook.com>
2021-01-10 15:56:30 +0800

    Problem 33 solved in Java.

--- LeetCode/java/33.search-in-rotated-sorted-array.java
+++ LeetCode/java/33.search-in-rotated-sorted-array.java
@@ -48,25 +48,33 @@
 // @lc code=start
 class Solution {
     public int search(int[] nums, int target) {
+        if(nums == null || nums.length <= 0)
+            return -1;
         int left = 0, right = nums.length - 1;
-        while (left <= right) {
+        while(left <= right){
             int mid = left + (right - left) / 2;
-            if (nums[mid] == target) {
+            if(nums[mid] == target)
                 return mid;
-            }
-            if (nums[mid] >= nums[left]) {
-                if (nums[mid] > target && target >= nums[left])
-                    right = mid - 1;
-                else
+            if(nums[mid] <= nums[right]){
+                // nums[mid] ~ nums[right] is ascending
+                // By judging whether target is between nums[mid] and nums[right]
+                // we can know whether to search the left part(nums[left] ~ nums[mid]) 
+                // or the right part(nums[mid] ~ nums[right]);
+                if(nums[mid] <= target && target <= nums[right]){
                     left = mid + 1;
-            } else {
-                if (nums[mid] < target && target <= nums[right])
-                    left = mid + 1;
-                else
+                }else{
                     right = mid - 1;
+                }
+            }else{
+                if(nums[left] <= target && target <= nums[mid]){
+                    right = mid - 1;
+                }else{
+                    left = mid + 1;
+                }
             }
         }
         return -1;
     }
+}
 }
 // @lc code=end
