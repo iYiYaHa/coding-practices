f1c57ee90f275ea7a5ea51527b1cdf9d3f49a1a3
iYiYaHa <zhangyjM@outlook.com>
2021-09-20 23:41:12 +0800

    Solve leetcode problem 203.

---
+++ lc-idea/leetcode/editor/en/[203]Remove Linked List Elements.java
@@ -0,0 +1,79 @@
+//Given the head of a linked list and an integer val, remove all the nodes of 
+//the linked list that has Node.val == val, and return the new head. 
+//
+// 
+// Example 1: 
+//
+// 
+//Input: head = [1,2,6,3,4,5,6], val = 6
+//Output: [1,2,3,4,5]
+// 
+//
+// Example 2: 
+//
+// 
+//Input: head = [], val = 1
+//Output: []
+// 
+//
+// Example 3: 
+//
+// 
+//Input: head = [7,7,7,7], val = 7
+//Output: []
+// 
+//
+// 
+// Constraints: 
+//
+// 
+// The number of nodes in the list is in the range [0, 10⁴]. 
+// 1 <= Node.val <= 50 
+// 0 <= val <= 50 
+// 
+// Related Topics Linked List Recursion 👍 3369 👎 135
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode removeElements(ListNode head, int val) {
+        if(head == null){
+            return head;
+        }
+        ListNode dummy = new ListNode(0, head);
+        ListNode prev = dummy;
+        while(prev.next != null){
+            ListNode ele = prev.next;
+            if(ele.val == val){
+                prev.next = ele.next;
+            }else {
+                prev = prev.next;
+            }
+        }
+        return dummy.next;
+    }
+}
+class Solution {
+    public ListNode removeElements(ListNode head, int val) {
+        if (head == null) {
+            return null;
+        }
+        if (head.val == val) {
+            return removeElements(head.next, val);
+        } else {
+            head.next = removeElements(head.next, val);
+            return head;
+        }
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
