904b7579177268db6b15761dbd3883c69eccd031
iYiYaHa <zhangyjM@outlook.com>
2021-09-25 20:38:13 +0800

    Improve the solution of codeforces 607B.

--- CodeForces/java/607B.java
+++ CodeForces/java/607B.java
@@ -14,17 +14,13 @@ public class Solution {
         for (int i = n - 1; i >= 0; --i) {
             dp[i][i] = 1;
             for (int j = i + 1; j < n; ++j) {
-                if (j == i + 1) {
-                    dp[i][j] = gems[i] == gems[j] ? 1 : 2;
-                } else {
-                    dp[i][j] = j - i + 1;
-                    for (int k = i; k <= j; ++k) // remove with gems[k]
-                    {
-                        if (gems[i] == gems[k]) {
-                            int left = (i == k || i + 1 == k) ? 1 : dp[i + 1][k - 1];
-                            int right = k + 1 <= j ? dp[k + 1][j] : 0;
-                            dp[i][j] = Math.min(dp[i][j], left + right);
-                        }
+                dp[i][j] = j - i + 1;
+                for (int k = i; k <= j; ++k) // remove with gems[k]
+                {
+                    if (gems[i] == gems[k]) {
+                        int left = (i == k || i + 1 == k) ? 1 : dp[i + 1][k - 1];
+                        int right = k + 1 <= j ? dp[k + 1][j] : 0;
+                        dp[i][j] = Math.min(dp[i][j], left + right);
                     }
                 }
             }
