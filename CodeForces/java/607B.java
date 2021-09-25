f2f4ed64875dc5ceab521fa73bb256c80d0cf71f
iYiYaHa <zhangyjM@outlook.com>
2021-09-25 19:52:49 +0800

    Solve codeforces problem 607B.

---
+++ CodeForces/java/607B.java
@@ -0,0 +1,44 @@
+
+// ref: https://www.cnblogs.com/hxer/p/5185143.html
+// ref: https://www.cnblogs.com/new-hand/p/7745437.html
+
+import java.util.*;
+
+public class Solution {
+    public static int solve(int[] gems) {
+        if (gems.length == 0) {
+            return 0;
+        }
+        int n = gems.length;
+        int[][] dp = new int[n][n];
+        for (int i = n - 1; i >= 0; --i) {
+            dp[i][i] = 1;
+            for (int j = i + 1; j < n; ++j) {
+                if (j == i + 1) {
+                    dp[i][j] = gems[i] == gems[j] ? 1 : 2;
+                } else {
+                    dp[i][j] = j - i + 1;
+                    for (int k = i; k <= j; ++k) // remove with gems[k]
+                    {
+                        if (gems[i] == gems[k]) {
+                            int left = (i == k || i + 1 == k) ? 1 : dp[i + 1][k - 1];
+                            int right = k + 1 <= j ? dp[k + 1][j] : 0;
+                            dp[i][j] = Math.min(dp[i][j], left + right);
+                        }
+                    }
+                }
+            }
+        }
+        return dp[0][n - 1];
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int[] gems = new int[n];
+        for (int i = 0; i < n; ++i) {
+            gems[i] = scanner.nextInt();
+        }
+        System.out.println(Solution.solve(gems));
+    }
+}
