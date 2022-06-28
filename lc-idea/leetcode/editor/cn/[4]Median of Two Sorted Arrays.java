e2ff0547b01b369c2a6112428fbb9414f8dbdcae
iYiYaHa <zhangyjM@outlook.com>
2022-06-28 23:26:57 +0800

    Solve leetcode problem 4.

--- lc-idea/leetcode/editor/cn/[4]Median of Two Sorted Arrays.java
+++ lc-idea/leetcode/editor/cn/[4]Median of Two Sorted Arrays.java
@@ -38,25 +38,21 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
         if (nums1 == null || nums2 == null) {
-            throw new IllegalArgumentException("Input array is null...");
+            throw new IllegalArgumentException("Input is null...");
         }
-        int totalLen = nums1.length + nums2.length;
-        if ((totalLen & 1) == 1) {
-            return findKthNumInTwoSortedArrays(nums1, nums2, totalLen / 2);
+        int cntEle = nums1.length + nums2.length;
+        if (cntEle % 2 != 0) {
+            return findKthEle(nums1, nums2, cntEle / 2);
         } else {
-            int kthEle = findKthNumInTwoSortedArrays(nums1, nums2, (totalLen / 2));
-            int k1thEle = findKthNumInTwoSortedArrays(nums1, nums2, (totalLen / 2 - 1));
-            System.out.println((totalLen / 2) + " "+(totalLen / 2 - 1));
-            System.out.println(kthEle + " " +k1thEle);
-            return (kthEle + k1thEle) / 2.0;
+            return (findKthEle(nums1, nums2, cntEle / 2) + findKthEle(nums1, nums2, cntEle / 2 - 1)) / 2.0D;
         }
     }
 
-    private int findKthNumInTwoSortedArrays(int[] nums1, int[] nums2, int k) {
-        return findKthNumInTwoSortedArraysCore(nums1, 0, nums1.length - 1, nums2, 0, nums2.length - 1, k);
+    private int findKthEle(int[] nums1, int[] nums2, int k) {
+        return findKthEle(nums1, 0, nums1.length - 1, nums2, 0, nums2.length - 1, k);
     }
 
-    private int findKthNumInTwoSortedArraysCore(int[] nums1, int leftA, int rightA, int[] nums2, int leftB, int rightB, int k) {
+    private int findKthEle(int[] nums1, int leftA, int rightA, int[] nums2, int leftB, int rightB, int k) {
         if (leftA > rightA) {
             return nums2[leftB + k];
         }
@@ -66,14 +62,44 @@ class Solution {
         if (k == 0) {
             return Math.min(nums1[leftA], nums2[leftB]);
         }
-
         int midA = Math.min(leftA + (k - 1) / 2, rightA);
         int midB = Math.min(leftB + (k - 1) / 2, rightB);
         if (nums1[midA] <= nums2[midB]) {
-            return findKthNumInTwoSortedArraysCore(nums1, midA + 1, rightA, nums2, leftB, rightB, k - (midA - leftA + 1));
+            return findKthEle(nums1, midA + 1, rightA, nums2, leftB, rightB, k - (midA - leftA + 1));
         } else {
-            return findKthNumInTwoSortedArraysCore(nums1, leftA, rightA, nums2, midB + 1, rightB, k - (midB - leftB + 1));
+            return findKthEle(nums1, leftA, rightA, nums2, midB + 1, rightB, k - (midB - leftB + 1));
         }
+    }
+}
+
+class Solution {
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        if (nums1 == null || nums2 == null) {
+            throw new IllegalArgumentException("null input...");
+        }
+        if (nums1.length > nums2.length) {
+            return findMedianSortedArrays(nums2, nums1);
+        }
+        int n = nums1.length;
+        int m = nums2.length;
+        int left = 0, right = n;
+        int median1 = 0, median2 = 0;
+        while (left <= right) {
+            int i = left + (right - left) / 2;
+            int j = (m + n + 1) / 2 - i;
+            int eleI = i < n ? nums1[i] : Integer.MAX_VALUE;
+            int eleJ = j < m ? nums2[j] : Integer.MAX_VALUE;
+            int eleIminusOne = i > 0 ? nums1[i - 1] : Integer.MIN_VALUE;
+            int eleJminusOne = j > 0 ? nums2[j - 1] : Integer.MIN_VALUE;
+            if (eleIminusOne <= eleJ) {
+                median1 = Math.max(eleIminusOne, eleJminusOne);
+                median2 = Math.min(eleI, eleJ);
+                left = i + 1;
+            } else {
+                right = i - 1;
+            }
+        }
+        return (n + m) % 2 == 0 ? (median1 + median2) / 2.0D : median1;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
