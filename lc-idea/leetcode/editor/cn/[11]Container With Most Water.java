709fae033d99df1ab97848a046206c40aa28a7eb
iYiYaHa <zhangyjM@outlook.com>
2022-10-22 23:04:39 +0800

    Solve leetcode problem 11.

---
+++ lc-idea/leetcode/editor/cn/[11]Container With Most Water.java
@@ -0,0 +1,63 @@
+//You are given an integer array height of length n. There are n vertical lines 
+//drawn such that the two endpoints of the iᵗʰ line are (i, 0) and (i, height[i]).
+// 
+//
+// Find two lines that together with the x-axis form a container, such that the 
+//container contains the most water. 
+//
+// Return the maximum amount of water a container can store. 
+//
+// Notice that you may not slant the container. 
+//
+// 
+// Example 1: 
+// 
+// 
+//Input: height = [1,8,6,2,5,4,8,3,7]
+//Output: 49
+//Explanation: The above vertical lines are represented by array [1,8,6,2,5,4,8,
+//3,7]. In this case, the max area of water (blue section) the container can 
+//contain is 49.
+// 
+//
+// Example 2: 
+//
+// 
+//Input: height = [1,1]
+//Output: 1
+// 
+//
+// 
+// Constraints: 
+//
+// 
+// n == height.length 
+// 2 <= n <= 10⁵ 
+// 0 <= height[i] <= 10⁴ 
+// 
+//
+// Related Topics 贪心 数组 双指针 👍 3852 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+
+    public int maxArea(int[] height) {
+        if (height == null || height.length <= 0) {
+            throw new IllegalArgumentException("Empty height...");
+        }
+        int ans = 0;
+        int left = 0, right = height.length - 1;
+        while (left < right) {
+            int area = (right - left) * Math.min(height[left], height[right]);
+            ans = Math.max(area, ans);
+            if (height[left] < height[right]) {
+                ++left;
+            } else {
+                --right;
+            }
+        }
+        return ans;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
